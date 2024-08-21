9f1388cde4397c6ee27d08d73ef3bbccc4f61b94
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-08-22 01:10:52 +0530

    Initialize the spring and vue project

---
+++ helpdesk-server/src/main/java/org/bitmonsters/helpdesk/HelpdeskServerApplication.java
@@ -0,0 +1,13 @@
+package org.bitmonsters.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskServerApplication.class, args);
+    }
+
+}
---
+++ helpdesk-server/src/test/java/org/bitmonsters/helpdesk/HelpdeskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bitmonsters.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
